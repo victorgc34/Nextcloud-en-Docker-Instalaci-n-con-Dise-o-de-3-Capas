a6cf5764fbc99f03332f0d120f1b6a2b7d8a1bed
victorgc34 <156658337+victorgc34@users.noreply.github.com>
2024-12-16 21:02:04 +0100

    Add files via upload

---
+++ galera/sqldir/basedatos.sh
@@ -0,0 +1,21 @@
+#$IP_MAS=rango de IPs de los servidores Web (no poner máscara de red ya que mysql no lo permite)
+#$db_passwd=contraseña_usuario_root_database
+#$DB_USER=es el usuario utilizado para la base de datos Wordpress
+#$DB_PASS=contraseña del usuario para la base de datos Wordpress
+
+DB_USER=user_owncloud
+DB_PASS=GHHJHSGDY
+
+IP_MAS=192.168.3.%
+#Esta debe de coincidir con la variable indicada en el docker-compose
+db_passwd=1234contrasena
+
+#######################
+#######################
+
+#Crea la base de datos para Wordpress (El nombre de la base de datos es wordpress_db)
+mariadb -u root -p$db_passwd -e "CREATE DATABASE owncloud_db;FLUSH PRIVILEGES;"
+
+# Creación de un nuevo usuario de MySQL y asignación de privilegios
+mariadb -u root -p$db_passwd -e "CREATE USER '$DB_USER'@'$IP_MAS' IDENTIFIED BY '$DB_PASS';"
+mariadb -u root -p$db_passwd -e "GRANT ALL PRIVILEGES ON owncloud_db.* TO '$DB_USER'@'$IP_MAS';FLUSH PRIVILEGES;"
